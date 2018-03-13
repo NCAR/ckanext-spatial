cc7ec7fde6ae0b34c280ff4067c52264b43629c5
sakhanUCAR <sakhan@ucar.edu>
2018-03-13 13:35:08 +0000

    DSET-304 Refactored map widget to expand by click event anywhere on the map.

--- ckanext/spatial/public/js/spatial_query.js
+++ ckanext/spatial/public/js/spatial_query.js
@@ -2,226 +2,232 @@
  */
 this.ckan.module('spatial-query', function ($, _) {
 
-  return {
-    options: {
-      i18n: {
-      },
-      style: {
-        color: '#F06F64',
-        weight: 2,
-        opacity: 1,
-        fillColor: '#F06F64',
-        fillOpacity: 0.1,
-        clickable: false
-      },
-      default_extent: [[90, 180], [-90, -180]]
-    },
-    template: {
-      buttons: [
-        '<div id="dataset-map-edit-buttons">',
-        //'<a href="javascript:;" class="btn cancel">Cancel</a> ',
-        '<a href="javascript:;" class="btn apply disabled">Search</a>',
-        '</div>'
-      ].join('')
-    },
-
-    initialize: function () {
-      var module = this;
-      $.proxyAll(this, /_on/);
-
-      var user_default_extent = this.el.data('default_extent');
-      if (user_default_extent ){
-        if (user_default_extent instanceof Array) {
-          // Assume it's a pair of coords like [[90, 180], [-90, -180]]
-          this.options.default_extent = user_default_extent;
-        } else if (user_default_extent instanceof Object) {
-          // Assume it's a GeoJSON bbox
-          this.options.default_extent = new L.GeoJSON(user_default_extent).getBounds();
-        }
-      }
-      this.el.ready(this._onReady);
-    },
-
-    _getParameterByName: function (name) {
-      var match = RegExp('[?&]' + name + '=([^&]*)')
-                        .exec(window.location.search);
-      return match ?
-          decodeURIComponent(match[1].replace(/\+/g, ' '))
-          : null;
-    },
-
-    _drawExtentFromCoords: function(xmin, ymin, xmax, ymax) {
-        if ($.isArray(xmin)) {
-            var coords = xmin;
-            xmin = coords[0]; ymin = coords[1]; xmax = coords[2]; ymax = coords[3];
-        }
-        return new L.Rectangle([[ymin, xmin], [ymax, xmax]],
-                               this.options.style);
-    },
-
-    _drawExtentFromGeoJSON: function(geom) {
-        return new L.GeoJSON(geom, {style: this.options.style});
-    },
-
-    _onReady: function() {
-      var module = this;
-      var map;
-      var extentLayer;
-      var previous_box;
-      var previous_extent;
-      var is_expanded = false;
-      var should_zoom = true;
-      var form = $("#dataset-search");
-      // CKAN 2.1
-      if (!form.length) {
-          form = $(".search-form");
-      }
-
-      var buttons;
-
-      // Add necessary fields to the search form if not already created
-      $(['ext_bbox', 'ext_prev_extent']).each(function(index, item){
-        if ($("#" + item).length === 0) {
-          $('<input type="hidden" />').attr({'id': item, 'name': item}).appendTo(form);
-        }
-      });
-
-      // OK map time
-      map = ckan.commonLeafletMap(
-        'dataset-map-container',
-        this.options.map_config,
-        {
-          attributionControl: false,
-          drawControlTooltips: false
-        }
-      );
-
-      // This is required to control transition between Pan/Drag Map and Draw Rectangle mode.
-      var rectangleDrawButton = new L.Draw.Rectangle(map, {shapeOptions: module.options.style});
-
-      //Button to enable Draw Rectangle mode
-      var drawRectangleButton = L.easyButton('&#11034;', function(btn, map){
-          rectangleDrawButton.enable();
-      });
-      // Button to enable Pan/Drag Map mode.
-      var panDragButton = L.easyButton('&#9995;', function(btn, map){
-          rectangleDrawButton.disable();
-      });
-
-      // Create a toolbar for Draw Rectangle and Pan/Drag Map buttons.
-      L.easyBar([drawRectangleButton, panDragButton]).addTo(map);
-
-      // Button to expand/compress the map.
-      var expandCompressMapButton  = L.easyButton({
-        id: 'expand-map-button',  // an id for the generated button
-        position: 'topright',      // inherited from L.Control -- the corner it goes in
-        type: 'replace',          // set to animate when you're comfy with css
-        leafletClasses: true,     // use leaflet classes to style the button?
-        states:[
-            {
-              stateName: 'not-expanded',
-              onClick: function(button, map){
-                $('body').addClass('dataset-map-expanded');
-                resetMap();
-                map.zoomIn();
-                button.state('expanded');
-              },
-              title: 'Expand map',
-              icon: '&Gt;'
+    return {
+        options: {
+            i18n: {},
+            style: {
+                color: '#F06F64',
+                weight: 2,
+                opacity: 1,
+                fillColor: '#F06F64',
+                fillOpacity: 0.1,
+                clickable: false
             },
-            {
-              stateName: 'expanded',
-              onClick: function(button, map){
-                $('body').removeClass('dataset-map-expanded');
-                resetMap();
-                map.zoomOut();
-                button.state('not-expanded');
-              },
-              title: 'Compress map',
-              icon: '&Lt;'
+            default_extent: [[90, 180], [-90, -180]]
+        },
+        template: {
+            buttons: [
+                '<div id="dataset-map-edit-buttons">',
+                '<a href="javascript:;" class="btn cancel">Cancel</a> ',
+                '<a href="javascript:;" class="btn apply disabled">Apply</a>',
+                '</div>'
+            ].join('')
+        },
+
+        initialize: function () {
+            var module = this;
+            $.proxyAll(this, /_on/);
+
+            var user_default_extent = this.el.data('default_extent');
+            if (user_default_extent) {
+                if (user_default_extent instanceof Array) {
+                    // Assume it's a pair of coords like [[90, 180], [-90, -180]]
+                    this.options.default_extent = user_default_extent;
+                } else if (user_default_extent instanceof Object) {
+                    // Assume it's a GeoJSON bbox
+                    this.options.default_extent = new L.GeoJSON(user_default_extent).getBounds();
+                }
             }
-          ]
-      });
-      expandCompressMapButton.addTo(map);
+            this.el.ready(this._onReady);
+        },
 
-      // Setup the expanded buttons
-      buttons = $(module.template.buttons).insertBefore('#dataset-map-attribution');
+        _getParameterByName: function (name) {
+            var match = RegExp('[?&]' + name + '=([^&]*)')
+                .exec(window.location.search);
+            return match ?
+                decodeURIComponent(match[1].replace(/\+/g, ' '))
+                : null;
+        },
 
-      // Handle the apply expanded action
-      $('.apply', buttons).on('click', function() {
-        if (extentLayer) {
-          $('body').removeClass('dataset-map-expanded');
-          resetMap();
-          // Eugh, hacky hack.
-          setTimeout(function() {
-            map.fitBounds(extentLayer.getBounds());
-            submitForm();
-          }, 200);
+        _drawExtentFromCoords: function (xmin, ymin, xmax, ymax) {
+            if ($.isArray(xmin)) {
+                var coords = xmin;
+                xmin = coords[0];
+                ymin = coords[1];
+                xmax = coords[2];
+                ymax = coords[3];
+            }
+            return new L.Rectangle([[ymin, xmin], [ymax, xmax]],
+                this.options.style);
+        },
+
+        _drawExtentFromGeoJSON: function (geom) {
+            return new L.GeoJSON(geom, {style: this.options.style});
+        },
+
+        _onReady: function () {
+
+            var module = this;
+            var map;
+            var extentLayer;
+            var previous_extent;
+            var is_expanded = false;
+            var should_zoom = true;
+            var form = $("#dataset-search");
+            // CKAN 2.1
+            if (!form.length) {
+                form = $(".search-form");
+            }
+
+            var buttons;
+
+            // Add necessary fields to the search form if not already created
+            $(['ext_bbox', 'ext_prev_extent']).each(function (index, item) {
+                if ($("#" + item).length === 0) {
+                    $('<input type="hidden" />').attr({'id': item, 'name': item}).appendTo(form);
+                }
+            });
+
+            // OK map time
+            map = ckan.commonLeafletMap(
+                'dataset-map-container',
+                this.options.map_config,
+                {
+                    attributionControl: false,
+                    drawControlTooltips: false
+                }
+            );
+
+            // This is required to control transition between Pan/Drag Map and Draw Rectangle mode.
+            var rectangleDrawButton = new L.Draw.Rectangle(map, {shapeOptions: module.options.style});
+
+            //Button to enable Draw Rectangle mode
+            var drawRectangleButton = L.easyButton('&#11034;', function (btn, map) {
+                expandMapIfNotExpanded();
+                rectangleDrawButton.enable();
+            });
+
+            // Button to enable Pan/Drag Map mode.
+            var panDragButton = L.easyButton('&#9995;', function (btn, map) {
+                expandMapIfNotExpanded();
+                rectangleDrawButton.disable();
+            });
+
+            // Create a toolbar for Draw Rectangle and Pan/Drag Map buttons.
+            L.easyBar([drawRectangleButton, panDragButton]).addTo(map);
+
+            // Setup the expanded buttons
+            buttons = $(module.template.buttons).insertBefore('#dataset-map-attribution');
+
+            // Handle the cancel expanded action
+            $('.cancel', buttons).on('click', function () {
+                $('body').removeClass('dataset-map-expanded');
+                if (extentLayer) {
+                    map.removeLayer(extentLayer);
+                }
+                setPreviousExtent();
+                setPreviousBBBox();
+                resetMap();
+                is_expanded = false;
+                if(previous_extent) {
+                    coords = previous_extent.split(',');
+                    map.fitBounds([[coords[1], coords[0]], [coords[3], coords[2]]]);
+                }
+                else {
+                    map.fitBounds(module.options.default_extent);
+                }
+            });
+
+            // Handle the apply expanded action
+            $('.apply', buttons).on('click', function () {
+                if (extentLayer) {
+                    $('body').removeClass('dataset-map-expanded');
+                    is_expanded = false;
+                    resetMap();
+                    // Eugh, hacky hack.
+                    setTimeout(function () {
+                        map.fitBounds(extentLayer.getBounds());
+                        submitForm();
+                    }, 200);
+                }
+            });
+
+            // When user finishes drawing the box, record it and add it to the map
+            map.on('draw:created', function (e) {
+                if (extentLayer) {
+                    map.removeLayer(extentLayer);
+                }
+                extentLayer = e.layer;
+                $('#ext_bbox').val(extentLayer.getBounds().toBBoxString());
+                map.addLayer(extentLayer);
+                $('.apply', buttons).removeClass('disabled').addClass('btn-primary');
+            });
+
+            // Record the current map view so we can replicate it after submitting
+            map.on('moveend', function (e) {
+                $('#ext_prev_extent').val(map.getBounds().toBBoxString());
+            });
+
+            // Ok setup the default state for the map
+            var previous_bbox;
+            setPreviousBBBox();
+            setPreviousExtent();
+
+            // OK, when we expand we shouldn't zoom then
+            map.on('zoomstart', function (e) {
+                should_zoom = false;
+            });
+
+
+            // Is there an existing box from a previous search?
+            function setPreviousBBBox() {
+                previous_bbox = module._getParameterByName('ext_bbox');
+                if (previous_bbox) {
+                    $('#ext_bbox').val(previous_bbox);
+                    extentLayer = module._drawExtentFromCoords(previous_bbox.split(','))
+                    map.addLayer(extentLayer);
+                    map.fitBounds(extentLayer.getBounds());
+                }
+            }
+
+            // Is there an existing extent from a previous search?
+            function setPreviousExtent() {
+                previous_extent = module._getParameterByName('ext_prev_extent');
+                if (previous_extent) {
+                    coords = previous_extent.split(',');
+                    map.fitBounds([[coords[1], coords[0]], [coords[3], coords[2]]]);
+                } else {
+                    if (!previous_bbox) {
+                        map.fitBounds(module.options.default_extent);
+                    }
+                }
+            }
+
+            // Reset map view
+            function resetMap() {
+                L.Util.requestAnimFrame(map.invalidateSize, map, !1, map._container);
+            }
+
+            // Add the loading class and submit the form
+            function submitForm() {
+                setTimeout(function () {
+                    form.submit();
+                }, 800);
+            }
+            // Expands the map and performs one level of zoom-in.
+            function expandMapIfNotExpanded() {
+                if (!is_expanded) {
+                    $('body').addClass('dataset-map-expanded');
+                    resetMap();
+                    map.zoomIn();
+                    is_expanded = true;
+                }
+            }
+
+            // Expand the map when smaller map is clicked.
+            $("#dataset-map-container").on('click', '*', function () {
+                expandMapIfNotExpanded();
+            });
         }
-      });
-
-      // When user finishes drawing the box, record it and add it to the map
-      map.on('draw:created', function (e) {
-        if (extentLayer) {
-          map.removeLayer(extentLayer);
-        }
-        extentLayer = e.layer;
-        $('#ext_bbox').val(extentLayer.getBounds().toBBoxString());
-        map.addLayer(extentLayer);
-        $('.apply', buttons).removeClass('disabled').addClass('btn-primary');
-      });
-
-      // Record the current map view so we can replicate it after submitting
-      map.on('moveend', function(e) {
-        $('#ext_prev_extent').val(map.getBounds().toBBoxString());
-      });
-
-      // Ok setup the default state for the map
-      var previous_bbox;
-      setPreviousBBBox();
-      setPreviousExtent();
-
-      // OK, when we expand we shouldn't zoom then
-      map.on('zoomstart', function(e) {
-        should_zoom = false;
-      });
-
-
-      // Is there an existing box from a previous search?
-      function setPreviousBBBox() {
-        previous_bbox = module._getParameterByName('ext_bbox');
-        if (previous_bbox) {
-          $('#ext_bbox').val(previous_bbox);
-          extentLayer = module._drawExtentFromCoords(previous_bbox.split(','))
-          map.addLayer(extentLayer);
-          map.fitBounds(extentLayer.getBounds());
-        }
-      }
-
-      // Is there an existing extent from a previous search?
-      function setPreviousExtent() {
-        previous_extent = module._getParameterByName('ext_prev_extent');
-        if (previous_extent) {
-          coords = previous_extent.split(',');
-          map.fitBounds([[coords[1], coords[0]], [coords[3], coords[2]]]);
-        } else {
-          if (!previous_bbox){
-              map.fitBounds(module.options.default_extent);
-          }
-        }
-      }
-
-      // Reset map view
-      function resetMap() {
-        L.Util.requestAnimFrame(map.invalidateSize, map, !1, map._container);
-      }
-
-      // Add the loading class and submit the form
-      function submitForm() {
-        setTimeout(function() {
-          form.submit();
-        }, 800);
-      }
     }
-  }
 });
